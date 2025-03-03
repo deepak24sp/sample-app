7156d2b7ed884a4cea3ae7f58269b6b7e4e0049c
deepak24sp <deepaksp@sujanix.com>
2025-03-03 17:57:33 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import sample from "./data/sample.json";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello world</p>
+        <ul>
+          {sample.map((item, index) => (
+            <li key={index}>{JSON.stringify(item.name)}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
